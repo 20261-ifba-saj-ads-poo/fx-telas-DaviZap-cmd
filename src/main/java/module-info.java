550833fcf4072b2209b97203ab75e9e89782dd6f
DaviZap-cmd <davimacedo987@gmail.com>
2026-06-09 19:20:58 -0300

    Update module-info.java
    ajustado

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module br.edu.ifba.saj.ads.poo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires transitive javafx.graphics;
+    requires transitive javafx.base;
 
     opens br.edu.ifba.saj.ads.poo to javafx.fxml;
     opens br.edu.ifba.saj.ads.poo.model to javafx.base;
