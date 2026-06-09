dc1cef86fd5133ee53729370177816009f0a3d87
DaviZap-cmd <davimacedo987@gmail.com>
2026-06-09 18:49:45 -0300

    Update and rename Cinema.java to  Hotel.java
    Cinema criado

---
+++ src/main/java/br/edu/ifba/saj/ads/poo/data/ Hotel.java
@@ -0,0 +1,9 @@
+package br.edu.ifba.saj.ads.poo.data;
+
+import java.util.ArrayList;
+import java.util.List;
+import br.edu.ifba.saj.ads.poo.model.Hotel;
+
+public class HotelData {
+    public static List<Hotel> hoteis = new ArrayList<>();
+}
